8f6086b0daaf456f8e1cb23d92c3b194247d04b8
Enir Ferraz <enirsilvaferraz@gmail.com>
2025-11-07 16:41:56 -0300

    feat: Add entity module
    This commit introduces the new `core:domain:entity` module. This module is configured as a KMP library and includes Koin for dependency injection.
    
    - **Module Setup:**
        - Created `core/domain/entity` module with a `build.gradle.kts` file.
        - Applied `foundation.project.library` and `foundation.library.koin` plugins.
        - Set the Android namespace to `com.eferraz.entities`.
    - **Dependency Injection:**
        - Added `EntityModule.kt` with Koin annotations (`@Module`, `@ComponentScan`) to set up dependency injection for the `com.eferraz.entity` package.
    - **Git:**
        - Added `.gitignore` to exclude the `/build` directory.

---
+++ core/domain/entity/build.gradle.kts
@@ -0,0 +1,10 @@
+import com.eferraz.buildlogic.scopes.library
+
+plugins {
+    alias(libs.plugins.foundation.project.library)
+    alias(libs.plugins.foundation.library.koin)
+}
+
+library {
+    namespace = "com.eferraz.entities"
+}
