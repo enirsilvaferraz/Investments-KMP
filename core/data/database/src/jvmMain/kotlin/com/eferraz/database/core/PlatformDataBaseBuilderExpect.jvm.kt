5379323978ba47517a738072b1304a6cbe33a6c0
Enir Ferraz <enirsilvaferraz@gmail.com>
2025-12-07 17:00:07 -0300

    chore: Change database location on desktop
    This commit changes the directory where the application database is stored on desktop (JVM) builds.
    
    Instead of using the system's temporary directory (`java.io.tmpdir`), the database file is now stored in a `Database` subfolder within the user's home directory (`user.home`).
    
    This ensures the database persists between application restarts and system reboots.

--- core/data/database/src/jvmMain/kotlin/com/eferraz/database/core/PlatformDataBaseBuilderExpect.jvm.kt
+++ core/data/database/src/jvmMain/kotlin/com/eferraz/database/core/PlatformDataBaseBuilderExpect.jvm.kt
@@ -7,7 +7,7 @@ import java.io.File
 internal actual object PlatformDataBaseBuilder : DataBaseBuilder {
 
     actual override fun buildPlatform(): RoomDatabase.Builder<AppDatabase> {
-        val dbFile = File(System.getProperty("java.io.tmpdir"), databaseName())
+        val dbFile = File(System.getProperty("user.home")+"/Database", databaseName())
         println("Enir: java.io.tmpdir: [$dbFile]")
         return Room.databaseBuilder<AppDatabase>(name = dbFile.absolutePath)
     }
