efa15d6087c347dfe6b0f6b5a9d0666e3c494340
Enir Francisco Silva Ferraz <enirsilvaferraz@gmail.com>
2026-05-19 11:19:31 -0300

    add naming module as compose one

--- core/presentation/composeApp/src/commonMain/kotlin/com/eferraz/presentation/helpers/Formatters.kt
+++ core/presentation/composeApp/src/commonMain/kotlin/com/eferraz/presentation/helpers/Formatters.kt
@@ -1,15 +1,7 @@
 package com.eferraz.presentation.helpers
 
-import com.eferraz.entities.assets.Asset
-import com.eferraz.entities.assets.FixedIncomeAsset
-import com.eferraz.entities.assets.FixedIncomeAssetType
-import com.eferraz.entities.assets.FixedIncomeSubType
 import com.eferraz.entities.assets.InvestmentCategory
-import com.eferraz.entities.assets.InvestmentFundAsset
-import com.eferraz.entities.assets.InvestmentFundAssetType
 import com.eferraz.entities.assets.Liquidity
-import com.eferraz.entities.assets.VariableIncomeAsset
-import com.eferraz.entities.assets.VariableIncomeAssetType
 import com.eferraz.entities.transactions.TransactionType
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.YearMonth
@@ -17,46 +9,6 @@ import kotlinx.datetime.format
 import kotlinx.datetime.format.char
 
 internal object Formatters {
-
-    internal fun Asset.formated(): String =
-        when (this) {
-            is FixedIncomeAsset -> "Renda Fixa"
-            is InvestmentFundAsset -> "Fundos" // "Fundo de Investimento"
-            is VariableIncomeAsset -> "Renda Variável"
-        }
-
-    internal fun FixedIncomeAssetType.formated() =
-        when (this) {
-            FixedIncomeAssetType.POST_FIXED -> "Pós Fixado"
-            FixedIncomeAssetType.PRE_FIXED -> "Prefixado"
-            FixedIncomeAssetType.INFLATION_LINKED -> "IPCA"
-        }
-
-    internal fun FixedIncomeSubType.formated() =
-        when (this) {
-            FixedIncomeSubType.CDB -> "CDB"
-            FixedIncomeSubType.LCI -> "LCI"
-            FixedIncomeSubType.LCA -> "LCA"
-            FixedIncomeSubType.LIG -> "LIG"
-            FixedIncomeSubType.DEBENTURE -> "Debênture"
-            FixedIncomeSubType.SELIC -> "SELIC"
-            FixedIncomeSubType.PRECATORIO -> "Precatório"
-        }
-
-    internal fun InvestmentFundAssetType.formated() =
-        when (this) {
-            InvestmentFundAssetType.PENSION -> "Previdência"
-            InvestmentFundAssetType.STOCK_FUND -> "Fundos de Ação"
-            InvestmentFundAssetType.MULTIMARKET_FUND -> "Multimercado"
-        }
-
-    internal fun VariableIncomeAssetType.formated() =
-        when (this) {
-            VariableIncomeAssetType.NATIONAL_STOCK -> "Ação"
-            VariableIncomeAssetType.INTERNATIONAL_STOCK -> "BDR"
-            VariableIncomeAssetType.REAL_ESTATE_FUND -> "FII"
-            VariableIncomeAssetType.ETF -> "ETF"
-        }
 
     internal fun LocalDate?.formated() =
         this?.format(
